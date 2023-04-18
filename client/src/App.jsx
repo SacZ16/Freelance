1f58b8f6f4717c27b3b600cd00a61aeda9fb29b0
alexis coronel <80395647+SacZ16@users.noreply.github.com>
2023-04-18 19:24:45 -0300

    "/"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/login" element={<Login/>}/>
-      <Route path="/detalle:id" element={<Detail/>}/>
+      <Route path="/detalle/:id" element={<Detail/>}/>
     </Routes>
     </BrowserRouter>
   )
