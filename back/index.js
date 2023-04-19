e33bf74ee1e1d8a62123f608329ad7623b0356cf
alexis coronel <80395647+SacZ16@users.noreply.github.com>
2023-04-19 18:41:07 -0300

    mercado pago

---
+++ back/index.js
@@ -0,0 +1,20 @@
+const express =require( "express");
+const cors =require( "cors");
+const http = require("http")
+const morgan =require( "morgan");
+const dotenv=require("dotenv")
+const indexRouter = require("./src/indexRouter")
+dotenv.config()
+// import { connectDB } from "./src/db/mongodb.js";
+// connectDB();
+const app = express();
+const server = http.createServer(app);
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/",indexRouter)
+server.listen(8080, () => {
+    console.log(`Server on port 8080`);
+  });
